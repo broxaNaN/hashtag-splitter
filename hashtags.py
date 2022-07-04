9f245146ec951454a8dc9cbb23f0750f33d1f531
broxaNaN <95721284+broxaNaN@users.noreply.github.com>
2022-07-05 00:48:09 +0300

    Update hashtags.py

--- hashtags.py
+++ hashtags.py
@@ -6,7 +6,12 @@ from collections import Counter
 
 def split_hashtag(text):
     probs, lasts = [1.0], [0]
-    text = text.replace(text[0], '')
+    ok = False
+    text = text.lower()
+    last_position = 0
+    if text[0] == '#':
+        text = text.replace(text[0], '')
+        ok = True
     final_list = []
     for i in range(1, len(text) + 1):
         prob_k, k = max((probs[j] * word_prob(text[j:i]), j)
@@ -21,17 +26,38 @@ def split_hashtag(text):
     words.reverse()
     for index in range(len(words)):
         test_dict = {}
-        sample = re.search(words[index], text)
+        sample = re.search(words[index], text[last_position:])
         sample_tuple = sample.span()
         test_dict['token'] = words[index]
-        test_dict['start'] = sample_tuple[0] + 1
-        test_dict['end'] = sample_tuple[1]
+        if ok == True:
+            test_dict['start'] = sample_tuple[0] + 1
+            test_dict['end'] = sample_tuple[1]
+        else:
+            test_dict['start'] = sample_tuple[0]
+            test_dict['end'] = sample_tuple[1] - 1
+        test_dict['start'] += last_position
+        test_dict['end'] += last_position
+        last_position = test_dict['end']
         final_list.append(test_dict)
     return final_list
 
-def word_prob(word): return dictionary[word] / total
-def words(text): return re.findall('[a-z]+', text.lower()) 
-dictionary = Counter(words(open('big.txt').read()))
+dictionary = {}
+file = open('example - Sheet1.tsv')
+fileLines = file.read().split('\n')
+
+for index in range(len(fileLines)):
+    key, value = fileLines[index].split('\t')
+    value = int(value)
+    dictionary[key] = value
+
+dictionary = Counter(dictionary)
+
+def word_prob(word): 
+    return dictionary[word] / total
+
+def words(text): 
+    return re.findall('[a-z]+', text.lower()) 
+
 max_word_length = max(map(len, dictionary))
 total = float(sum(dictionary.values()))
 
