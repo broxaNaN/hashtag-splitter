d03427effccc122f4604afac4bf6a2f6a51c394f
broxaNaN <95721284+broxaNaN@users.noreply.github.com>
2022-08-31 15:41:52 +0300

    Update hashtags.py

--- hashtags.py
+++ hashtags.py
@@ -3,36 +3,32 @@
 # adapted to the Romanian language. 
 
 import re
+import json
 from collections import Counter
 from arguments import getArguments
 
 args = getArguments()
 
-def replace_diacritice(s):
+def replace_diacritics(s):
     s = s.replace("ă","a").replace("î","i").replace("â","a").replace("ș","s").replace("ț","t")
     if args.lower == "Yes":
         return s
     if args.lower == "No":
         return s.replace("Ă","A").replace("Î","I").replace("Â","A").replace("Ș","S").replace("Ț","T")
 
-def upper_letters(hashtag):
-    list = []
-    for index in range(len(hashtag)):
-        if hashtag[index].isupper():
-            list.append(index)
-    return list
-
-def split_hashtag ():
-    probs, lasts = [1.0], [0]
+def split_hashtag():
+    probs, lasts = [1.0], [0] 
     ok = False
     text = args.hashtag
     text = text.replace(text[0], '')
-    if args.lower == "No":
-        found_index = upper_letters(text)
-    text = text.lower()
+    original_text = text
+    if args.lower == "Yes":
+        text = text.lower()
     if args.remove_diacritics == "Yes":
-        text = replace_diacritice(text)
+        text = replace_diacritics(text)
     last_position = 0
+    if ok == True:
+        last_position = 1
     if text[0] == '#':
         text = text.replace(text[0], '')
         ok = True
@@ -50,18 +46,9 @@ def split_hashtag ():
     words.reverse()
     for index in range(len(words)):
         word_dict = {}
-        sample = re.search(words[index], text[last_position:])
+        sample = re.search(words[index], text[last_position:]) 
         sample_tuple = sample.span()
         found_word = words[index]
-        if args.lower == "No":
-            if ok == True:
-                for index_2 in range(len(found_index)):
-                    found_word = found_word.replace(found_word[found_index[index_2] - 1],
-                                                    found_word[found_index[index_2] - 1].upper())
-            else:
-                for index_2 in range(len(found_index)):
-                    found_word = found_word.replace(found_word[found_index[index_2]],
-                                                    found_word[found_index[index_2]].upper())
         word_dict['token'] = found_word
         if ok == True:
             word_dict['start'] = sample_tuple[0] + 1
@@ -71,6 +58,7 @@ def split_hashtag ():
             word_dict['end'] = sample_tuple[1] - 1
         word_dict['start'] += last_position
         word_dict['end'] += last_position
+        word_dict['token'] = original_text[word_dict['start']:word_dict['end'] + 1]
         last_position = word_dict['end']
         final_list.append(word_dict)
     return final_list
@@ -80,6 +68,11 @@ file = args.frequency_file
 fileLines = file.read().split('\n')
 
 for index in range(len(fileLines) - 1):
+    if args.process_frequency_list == "Yes":
+        if args.lower == "Yes":
+            fileLines[index] = fileLines[index].lower()
+        if args.remove_diacritics == "Yes":
+            replace_diacritics(fileLines[index])
     key, value = fileLines[index].split('\t')
     value = int(value)
     dictionary[key] = value
@@ -92,4 +85,4 @@ def word_prob(word):
 max_word_length = max(map(len, dictionary))
 total = float(sum(dictionary.values()))
 
-print(split_hashtag())
+print(json.dumps(split_hashtag()))
